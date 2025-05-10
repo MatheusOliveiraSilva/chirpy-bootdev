445a6b55e0c01337f25992b6aaf86951685e9684
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-05-10 06:02:53 -0300

    adding image and fileserve handlers

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("/assets")))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
